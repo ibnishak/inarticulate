4d55507551db8b3e4dbd1ff9212249a149adb77f
Rizwan Ishak <ibnishak@live.com>
2023-08-17 20:44:53 +0530

    [chore](dev) Adding config files

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+const purgecss = require("@fullhuman/postcss-purgecss");
+module.exports = {
+  plugins: [
+    require("autoprefixer"),
+    require("cssnano")({
+      preset: "default",
+    }),
+  ],
+};
